2158a1c5e3d6bdfb9cc497480671acdaca390838
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2023-10-23 10:52:54 -0400

    Add colors to ensure contrast on hover/splash

--- lib/safe_colors.dart
+++ lib/safe_colors.dart
@@ -7,37 +7,53 @@ import 'package:libmonet/hct.dart';
 
 class SafeColors {
   final Color color;
-  final Color colorBorder;
-  final Color colorText;
-  final Color colorIcon;
   final Color colorHover;
   final Color colorSplash;
 
+  final Color colorText;
+  final Color colorHoverText;
+  final Color colorSplashText;
+
+  final Color colorIcon;
+  final Color colorBorder;
+
   final Color fill;
-  final Color fillText;
-  final Color fillIcon;
   final Color fillHover;
   final Color fillSplash;
 
+  final Color fillHoverText;
+  final Color fillSplashText;
+
+  final Color fillText;
+  final Color fillIcon;
+
   final Color text;
   final Color textHover;
+  final Color textHoverText;
   final Color textSplash;
+  final Color textSplashText;
 
   SafeColors({
     required this.color,
-    required this.colorBorder,
     required this.colorText,
+    required this.colorBorder,
     required this.colorIcon,
     required this.colorHover,
+    required this.colorHoverText,
+    required this.colorSplashText,
     required this.colorSplash,
     required this.fill,
     required this.fillText,
+    required this.fillHoverText,
+    required this.fillSplashText,
     required this.fillIcon,
     required this.fillHover,
     required this.fillSplash,
     required this.text,
     required this.textHover,
+    required this.textHoverText,
     required this.textSplash,
+    required this.textSplashText,
   });
 
   factory SafeColors.from(
@@ -46,32 +62,40 @@ class SafeColors {
     double contrast = 0.5,
     Algo algo = Algo.apca,
   }) {
+    final hoverContrast = math.max(contrast - 0.3, 0.1);
+    final splashContrast = math.max(contrast - 0.15, 0.25);
+
     final colorHct = Hct.fromColor(color);
     bool needBorder = false;
     switch (algo) {
       case Algo.wcag21:
-        final requiredContrastRatio = contrastRatioInterpolation(percent: contrast, usage: Usage.fill);
-        final actualContrastRatio = contrastRatioOfLstars(colorHct.tone, backgroundLstar);
+        final requiredContrastRatio =
+            contrastRatioInterpolation(percent: contrast, usage: Usage.fill);
+        final actualContrastRatio =
+            contrastRatioOfLstars(colorHct.tone, backgroundLstar);
         if (actualContrastRatio < requiredContrastRatio) {
           needBorder = true;
         }
         break;
       case Algo.apca:
-        final  apca = apcaContrastOfApcaY(lstarToApcaY(colorHct.tone), lstarToApcaY(backgroundLstar));
-        final requiredApca = apcaInterpolation(percent: contrast, usage: Usage.fill);
+        final apca = apcaContrastOfApcaY(
+            lstarToApcaY(colorHct.tone), lstarToApcaY(backgroundLstar));
+        final requiredApca =
+            apcaInterpolation(percent: contrast, usage: Usage.fill);
         if (apca.abs() < requiredApca.abs()) {
           needBorder = true;
         }
         break;
     }
-  
-    final colorBorder = !needBorder ? colorHct.tone : contrastingLstar(
-      withLstar: backgroundLstar,
-      usage: Usage.fill,
-      by: algo,
-      contrast: contrast,
 
-    );
+    final colorBorder = !needBorder
+        ? colorHct.tone
+        : contrastingLstar(
+            withLstar: backgroundLstar,
+            usage: Usage.fill,
+            by: algo,
+            contrast: contrast,
+          );
     final colorText = contrastingLstar(
       withLstar: colorHct.tone,
       usage: Usage.text,
@@ -81,6 +105,30 @@ class SafeColors {
     final colorIcon = contrastingLstar(
       withLstar: colorHct.tone,
       usage: Usage.fill,
+      by: algo,
+      contrast: contrast,
+    );
+    final colorHover = contrastingLstar(
+      withLstar: colorHct.tone,
+      usage: Usage.fill,
+      by: algo,
+      contrast: hoverContrast,
+    );
+    final colorSplash = contrastingLstar(
+      withLstar: colorHct.tone,
+      usage: Usage.fill,
+      by: algo,
+      contrast: splashContrast,
+    );
+    final colorTextSplash = contrastingLstar(
+      withLstar: colorSplash,
+      usage: Usage.text,
+      by: algo,
+      contrast: contrast,
+    );
+    final colorTextHover = contrastingLstar(
+      withLstar: colorHover,
+      usage: Usage.text,
       by: algo,
       contrast: contrast,
     );
@@ -98,27 +146,7 @@ class SafeColors {
       by: algo,
       contrast: contrast,
     );
-    final text = contrastingLstar(
-      withLstar: backgroundLstar,
-      usage: Usage.text,
-      by: algo,
-      contrast: contrast,
-    );
 
-    final hoverContrast = math.max(contrast - 0.3, 0.1);
-    final splashContrast = math.max(contrast - 0.15, 0.1);
-    final colorHover = contrastingLstar(
-      withLstar: colorHct.tone,
-      usage: Usage.fill,
-      by: algo,
-      contrast: hoverContrast,
-    );
-    final colorSplash = contrastingLstar(
-      withLstar: colorHct.tone,
-      usage: Usage.fill,
-      by: algo,
-      contrast: splashContrast,
-    );
     final fillHover = contrastingLstar(
       withLstar: fill,
       usage: Usage.fill,
@@ -130,6 +158,25 @@ class SafeColors {
       usage: Usage.fill,
       by: algo,
       contrast: splashContrast,
+    );
+    final fillTextHover = contrastingLstar(
+      withLstar: fillHover,
+      usage: Usage.text,
+      by: algo,
+      contrast: contrast,
+    );
+    final fillTextSplash = contrastingLstar(
+      withLstar: fillSplash,
+      usage: Usage.text,
+      by: algo,
+      contrast: contrast,
+    );
+
+    final text = contrastingLstar(
+      withLstar: backgroundLstar,
+      usage: Usage.text,
+      by: algo,
+      contrast: contrast,
     );
     final textHover = contrastingLstar(
       withLstar: backgroundLstar,
@@ -143,23 +190,41 @@ class SafeColors {
       by: algo,
       contrast: splashContrast,
     );
+    final textHoverText = contrastingLstar(
+      withLstar: textHover,
+      usage: Usage.text,
+      by: algo,
+      contrast: contrast,
+    );
+    final textSplashText = contrastingLstar(
+      withLstar: textSplash,
+      usage: Usage.text,
+      by: algo,
+      contrast: contrast,
+    );
     final hue = colorHct.hue;
     final chroma = colorHct.chroma;
     return SafeColors(
       color: color,
-      colorBorder: Hct.colorFrom(hue, chroma, colorBorder),
-      colorText: Hct.colorFrom(hue, chroma, colorText),
-      colorIcon: Hct.colorFrom(hue, chroma, colorIcon),
       colorHover: Hct.colorFrom(hue, chroma, colorHover),
       colorSplash: Hct.colorFrom(hue, chroma, colorSplash),
+      colorBorder: Hct.colorFrom(hue, chroma, colorBorder),
+      colorText: Hct.colorFrom(hue, chroma, colorText),
+      colorHoverText: Hct.colorFrom(hue, chroma, colorTextHover),
+      colorSplashText: Hct.colorFrom(hue, chroma, colorTextSplash),
+      colorIcon: Hct.colorFrom(hue, chroma, colorIcon),
       fill: Hct.colorFrom(hue, chroma, fill),
       fillText: Hct.colorFrom(hue, chroma, fillText),
+      fillHoverText: Hct.colorFrom(hue, chroma, fillTextHover),
+      fillSplashText: Hct.colorFrom(hue, chroma, fillTextSplash),
       fillIcon: Hct.colorFrom(hue, chroma, fillIcon),
       fillHover: Hct.colorFrom(hue, chroma, fillHover),
       fillSplash: Hct.colorFrom(hue, chroma, fillSplash),
       text: Hct.colorFrom(hue, chroma, text),
       textHover: Hct.colorFrom(hue, chroma, textHover),
+      textHoverText: Hct.colorFrom(hue, chroma, textHoverText),
       textSplash: Hct.colorFrom(hue, chroma, textSplash),
+      textSplashText: Hct.colorFrom(hue, chroma, textSplashText),
     );
   }
 }
--- lib/theming/button_style.dart
+++ lib/theming/button_style.dart
@@ -18,69 +18,89 @@ extension SafeColorsButtonStyle on ButtonStyle {
       foregroundColor: MaterialStateProperty.all(safeColors.colorText),
       shadowColor: MaterialStateProperty.all(safeColors.colorBorder),
       elevation: MaterialStateProperty.all(0),
-      side:
-          MaterialStateProperty.all(BorderSide(color: safeColors.colorBorder, width: 2)),
+      side: MaterialStateProperty.all(
+          BorderSide(color: safeColors.colorBorder, width: 2)),
     );
   }
 }
 
+MaterialStateProperty<Color> stateColors(
+    {required Color color, required Color hover, required Color splash}) {
+  return MaterialStateProperty.resolveWith((states) {
+    if (states.contains(MaterialState.pressed)) {
+      return splash;
+    } else if (states.contains(MaterialState.focused) ||
+        states.contains(MaterialState.hovered)) {
+      return hover;
+    }
+    return color;
+  });
+}
+
 ButtonStyle buttonStylefromSafeColorsColor(SafeColors safeColors) {
+  final alwaysSurface = stateColors(
+    color: safeColors.color,
+    hover: safeColors.color,
+    splash: safeColors.color,
+  );
   return ButtonStyle(
-    backgroundColor: MaterialStateProperty.all(safeColors.color),
-    overlayColor: MaterialStateProperty.resolveWith((states) {
-      if (states.contains(MaterialState.hovered)) {
-        return safeColors.colorHover;
-      } else if (states.contains(MaterialState.pressed)) {
-        return safeColors.colorSplash;
-      } else if (states.contains(MaterialState.focused)) {
-        return safeColors.colorHover;
-      }
-      return null;
-    }),
-    foregroundColor: MaterialStateProperty.all(safeColors.colorText),
-    shadowColor: MaterialStateProperty.all(safeColors.colorBorder),
-    elevation: MaterialStateProperty.all(0),
-    side: MaterialStateProperty.all(BorderSide(color: safeColors.colorBorder, width: 2)),
+    backgroundColor: alwaysSurface,
+    surfaceTintColor: alwaysSurface,
+    overlayColor: stateColors(
+      color: safeColors.color,
+      hover: safeColors.colorHover,
+      splash: safeColors.colorSplash,
+    ),
+    foregroundColor: stateColors(
+      color: safeColors.colorText,
+      hover: safeColors.colorHoverText,
+      splash: safeColors.colorSplashText,
+    ),
+    side: MaterialStateProperty.all(
+      BorderSide(color: safeColors.colorBorder, width: 2),
+    ),
   );
 }
 
 ButtonStyle buttonStylefromSafeColorsFill(SafeColors safeColors) {
+  final alwaysSurface = stateColors(
+    color: safeColors.fill,
+    hover: safeColors.fill,
+    splash: safeColors.fill,
+  );
   return ButtonStyle(
-    backgroundColor: MaterialStateProperty.all(safeColors.fill),
-    overlayColor: MaterialStateProperty.resolveWith((states) {
-      if (states.contains(MaterialState.hovered)) {
-        return safeColors.fillHover;
-      } else if (states.contains(MaterialState.pressed)) {
-        return safeColors.fillSplash;
-      } else if (states.contains(MaterialState.focused)) {
-        return safeColors.fillHover;
-      }
-      return null;
-    }),
-    foregroundColor: MaterialStateProperty.all(safeColors.fillText),
-    shadowColor: MaterialStateProperty.all(safeColors.colorBorder),
-    elevation: MaterialStateProperty.all(0),
-    side: MaterialStateProperty.all(BorderSide(color: safeColors.fill)),
+    backgroundColor: alwaysSurface,
+    surfaceTintColor: alwaysSurface,
+    overlayColor: stateColors(
+      color: safeColors.fill,
+      hover: safeColors.fillHover,
+      splash: safeColors.fillSplash,
+    ),
+    foregroundColor: stateColors(
+      color: safeColors.fillText,
+      hover: safeColors.fillHoverText,
+      splash: safeColors.fillSplashText,
+    ),
+    side: MaterialStateProperty.all(
+      BorderSide(color: safeColors.fill, width: 2),
+    ),
   );
 }
-
 
 ButtonStyle buttonStylefromSafeColorsText(SafeColors safeColors) {
   return ButtonStyle(
     backgroundColor: MaterialStateProperty.all(null),
-    overlayColor: MaterialStateProperty.resolveWith((states) {
-      if (states.contains(MaterialState.hovered)) {
-        return safeColors.textHover;
-      } else if (states.contains(MaterialState.pressed)) {
-        return safeColors.textSplash;
-      } else if (states.contains(MaterialState.focused)) {
-        return safeColors.textHover;
-      }
-      return null;
-    }),
-    foregroundColor: MaterialStateProperty.all(safeColors.text),
-    shadowColor: MaterialStateProperty.all(null),
-    elevation: MaterialStateProperty.all(0),
+    surfaceTintColor: MaterialStateProperty.all(null),
+    overlayColor: stateColors(
+      color: Colors.transparent,
+      hover: safeColors.textHover,
+      splash: safeColors.textSplash,
+    ),
+    foregroundColor: stateColors(
+      color: safeColors.text,
+      hover: safeColors.textHoverText,
+      splash: safeColors.textSplashText,
+    ),
     side: MaterialStateProperty.all(BorderSide.none),
   );
 }
