4b5bb766294d6b4d5676d7c8a7d6ccd87cd304ce
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2023-10-30 10:52:27 -0400

    Slider thumb with letter

--- lib/theming/slider_flat_thumb.dart
+++ lib/theming/slider_flat_thumb.dart
@@ -5,11 +5,21 @@ import 'package:flutter/material.dart';
 class SliderFlatThumb extends SliderComponentShape {
   final double thumbRadius;
   final IconData? iconData;
+  final String? letter;
+  final TextStyle? letterTextStyle;
   final Color borderColor;
   final double borderWidth;
   final Color? iconColor;
 
-  SliderFlatThumb({this.thumbRadius = 18, this.iconData, this.iconColor, required this.borderColor, required this.borderWidth});
+  SliderFlatThumb({
+    this.thumbRadius = 18,
+    this.iconData,
+    this.iconColor,
+    this.letter,
+    this.letterTextStyle,
+    required this.borderColor,
+    required this.borderWidth,
+  });
 
   @override
   Size getPreferredSize(bool isEnabled, bool isDiscrete) {
@@ -34,7 +44,8 @@ class SliderFlatThumb extends SliderComponentShape {
     final paint = Paint()..color = colorTween.evaluate(enableAnimation)!;
     canvas.drawCircle(center, thumbRadius, paint);
     canvas.drawArc(
-      Rect.fromCenter(center: center, width: 2 * thumbRadius, height: 2 * thumbRadius)
+      Rect.fromCenter(
+              center: center, width: 2 * thumbRadius, height: 2 * thumbRadius)
           .deflate(borderWidth / 2),
       math.pi / 2,
       -math.pi,
@@ -44,22 +55,43 @@ class SliderFlatThumb extends SliderComponentShape {
         ..strokeWidth = borderWidth
         ..color = borderColor,
     );
-    final concreteIconData = iconData;
-    if (concreteIconData == null) return;
 
-    // Paint icon
+    final concreteIconData = iconData;
+    final paintIcon = concreteIconData != null;
+    final concreteTextStyle = letterTextStyle;
+    final concreteLetter = letter;
+    final paintLetter = concreteTextStyle != null && concreteLetter != null;
+
+    if (!paintIcon && !paintLetter) return;
     final ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
     final Canvas iconCanvas = Canvas(pictureRecorder);
-    final double iconSize = thumbRadius * 2 - 16; // Change as needed
-    final ui.ParagraphBuilder builder = ui.ParagraphBuilder(ui.ParagraphStyle(
-      fontFamily: concreteIconData.fontFamily,
-      fontSize: iconSize,
-    ))
-      ..pushStyle(ui.TextStyle(
-        color: iconColor, // specify the colour you wish here
+    final double iconSize = thumbRadius * 2 - 16;
+    final ui.ParagraphBuilder builder;
+    if (paintIcon) {
+      builder = ui.ParagraphBuilder(ui.ParagraphStyle(
+        fontFamily: concreteIconData.fontFamily,
         fontSize: iconSize,
       ))
-      ..addText(String.fromCharCode(concreteIconData.codePoint));
+        ..pushStyle(ui.TextStyle(
+          color: iconColor,
+          fontSize: iconSize,
+        ))
+        ..addText(String.fromCharCode(concreteIconData.codePoint));
+
+    } else if (paintLetter) {
+      builder = ui.ParagraphBuilder(ui.ParagraphStyle(
+        fontFamily: concreteTextStyle.fontFamily,
+        fontSize: iconSize,
+      ))
+        ..pushStyle(ui.TextStyle(
+          color: concreteTextStyle.color,
+          fontSize: iconSize,
+          fontWeight: concreteTextStyle.fontWeight,
+        ))
+        ..addText(concreteLetter);
+    } else {
+      throw Exception('Should not be here');
+    }
     final ui.Paragraph paragraph = builder.build()
       ..layout(ui.ParagraphConstraints(width: iconSize));
     final double iconHeight = paragraph.height;
