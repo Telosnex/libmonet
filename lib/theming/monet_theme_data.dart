b40a81838ac0cda998613264169ab8dd11c68d6f
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-11-12 20:25:48 -0500

    Try setting strut style to get more accurate font heights

--- lib/theming/monet_theme_data.dart
+++ lib/theming/monet_theme_data.dart
@@ -1522,6 +1522,7 @@ class MonetThemeData {
         // String is designed to be short and do a good job of capturing an accurate picture
         // of the extremes of the font's letter heights.
         text: TextSpan(text: '|&"qjQJAEIOUYaeiouy', style: textStyle),
+        strutStyle: StrutStyle.fromTextStyle(textStyle, forceStrutHeight: true, height: 1.0),
         maxLines: 1,
         textDirection: TextDirection.ltr,
       )..layout();
